44f8789372fdc0f0f217318c091092710f47bfae
Iain R. Moodie <38960700+irmoodie@users.noreply.github.com>
2026-01-12 17:09:07 +0100

    handle cases of poorly informative loci better

--- R/ImportCervusALF.R
+++ R/ImportCervusALF.R
@@ -15,7 +15,7 @@ ImportCervusALF <- function(ALFSummaryFile){
   # Isolate the summary statistics and coerce into a dataframe
   LociSummary <- read.table(
     text = summaryfile[(grep("\\*\\*\\*\\* Summary statistics \\*\\*\\*\\*", summaryfile)+3):(grep("Number of individuals:", summaryfile)-2)],
-    header = TRUE)
+    header = TRUE, na.strings = "ND")
   
   # Isolate the dataset statistics and coerce into a dataframe
   DataSummary <- stringr::str_split(
@@ -34,27 +34,37 @@ ImportCervusALF <- function(ALFSummaryFile){
   # create lists for per locus stats
   PerLocusAlleleFrequencies <- list()
   PerLocusStatistics <- list()
+
+  ReadPerLocusAlleleFrequencies <- function(x, k, r, col_names){
+    text <- x[r[-(1:3)][1:k]]
+    text <- gsub("Not done", "ND", text)
+    return(read.table(text = text, col.names = col_names, na.strings = "ND"))
+  }
   
   for (loci_n in 1:length(x)) {
     
     LociName <- LociSummary$Locus[loci_n]
     k_alleles <- LociSummary$k[loci_n]
     cols <- c("Allele", "Count", "Hets", "Homs", "Freq", "FreqWithNull")
-    
+
     # extract the allele frequency table for each loci
     if (loci_n != length(x)) {
       range <- (x[[loci_n]]):(x[[loci_n+1]])
-      PerLocusAlleleFrequencies[[LociName]] <- read.table(text = summaryfile[range[-(1:3)][1:k_alleles]], col.names = cols)
     }
-    
-    # extract the allele frequency table for the final loci in the dataset
     if (loci_n == length(x)) {
       range <- (x[[loci_n]]):(grep("\\*\\*\\*\\*\\*\\*\\*\\*", summaryfile))
-      PerLocusAlleleFrequencies[[LociName]] <- read.table(text = summaryfile[range[-(1:3)][1:k_alleles]], col.names = cols)
+    }
+    if (k_alleles > 0) {
+      PerLocusAlleleFrequencies[[LociName]] <- 
+        ReadPerLocusAlleleFrequencies(x = summaryfile, r = range, k = k_alleles, col_names = cols)
+    }
+    if (k_alleles == 0) {
+      PerLocusAlleleFrequencies[[LociName]] <- data.frame(matrix(NA, ncol = length(cols), nrow = 1))
+      colnames(PerLocusAlleleFrequencies[[LociName]]) <- cols
     }
     
     # extract the statistics table for each loci
-    stats_start <- 3 + k_alleles + 1
+    stats_start <- 3 + k_alleles + 2
     null_allele_line <- grep("Null allele frequency estimate", summaryfile[range])
     stats_lines <- summaryfile[range[stats_start:null_allele_line]]
     
@@ -63,6 +73,7 @@ ImportCervusALF <- function(ALFSummaryFile){
       pattern = ":",
       simplify = TRUE)
     PerLocusStatistics[[LociName]][,2] <- stringr::str_remove_all(string = PerLocusStatistics[[LociName]][,2], pattern = " ")
+    PerLocusStatistics[[LociName]][,2] <- stringr::str_replace_all(string = PerLocusStatistics[[LociName]][,2], pattern = "Notdone", replacement = NA_character_)
     PerLocusStatistics[[LociName]] <- data.frame(stat = PerLocusStatistics[[LociName]][,1], value = PerLocusStatistics[[LociName]][,2])
     
   }
@@ -78,6 +89,5 @@ ImportCervusALF <- function(ALFSummaryFile){
       "PerLocusStatistics" = PerLocusStatistics
     )
   )
-  
   return(CervusALFSummary)
 }
